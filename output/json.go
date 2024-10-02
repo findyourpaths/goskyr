859861910325e2427daaef46a949e088c9cadc06
Earl J. Wagner <wag@findyourpaths.com>
2024-10-02 19:45:56 +0200

    Adds "output" directory to version control.

---
+++ output/json.go
@@ -0,0 +1,26 @@
+package output
+
+import (
+	"encoding/json"
+	"fmt"
+
+	"golang.org/x/exp/slog"
+)
+
+type JSONWriter struct{}
+
+func (s *JSONWriter) Write(items chan map[string]interface{}) {
+	logger := slog.With(slog.String("writer", STDOUT_WRITER_TYPE))
+
+	items2 := []map[string]interface{}{}
+	for item := range items {
+		items2 = append(items2, item)
+	}
+
+	content, err := json.MarshalIndent(items2, "", "  ")
+	if err != nil {
+		logger.Error(fmt.Sprintf("error while writing items: %v", err))
+		return
+	}
+	fmt.Print(string(content))
+}
